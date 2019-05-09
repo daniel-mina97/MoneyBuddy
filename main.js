5c8f3de18db8655347967ba3b2138f1a9c834bc8
Daniel Mina <minadaniel27@gmail.com>
2019-05-09 00:08:51 -0500

    Show BrowserWindow only when ready
    Initialized the 'show' value of the Browser window to false. Now, a 'read-to-show' event wil trigger and show the window.
    
    Also changed the rest of the callbacks to use the fat arrow.

--- main.js
+++ main.js
@@ -8,25 +8,27 @@ function createWindow() {
         height: 800,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        show: false
     });
 
     mainWindow.loadFile('app/html/index.html');
+    mainWindow.once('ready-to-show', () => {mainWindow.show()});
 
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', () => {
         mainWindow = null;
     });
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on('activate', function() {
+app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }

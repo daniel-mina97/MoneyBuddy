04b7a453fc091f49b487f9b96e52c67cdad809be
Daniel Mina <minadaniel27@gmail.com>
2019-05-18 11:01:38 -0500

    Put forms in modal windows
    Instead of being on the same page, the enter expenses form will be on its own modal window. Likewise, the view
    expenses form will be on its own modal window. These modal windows will be opened when clicking on their
    respective button on the side menu.

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -2,10 +2,8 @@ const {remote} = require('electron');
 const SqlDriver = require('./sqlDriver');
 const sqlDriver = new SqlDriver(remote.app.getPath('userData'));
 
-console.log(sqlDriver);
-
-document.querySelector("h1").addEventListener("click", () => {
-    console.log("h1 got clicked.");
+document.querySelector("#enter-expense-submit").addEventListener("click", () => {
+    document.querySelector("#enter-expense-submit-hidden").click();
 });
 
 document.querySelector("#submit-expense").addEventListener("submit", (event) => {
@@ -15,13 +13,36 @@ document.querySelector("#submit-expense").addEventListener("submit", (event) => 
     let location = event.target[2].value;
     let description = event.target[3].value;
     sqlDriver.enterExpense(date, amount, location, description);
+    document.querySelector("#enter-expense-modal").classList.remove("is-active");
 });
 
-document.querySelector("#get-expenses").addEventListener("submit", (event) => {
+document.querySelector("#view-expenses-submit").addEventListener("click", () => {
+    document.querySelector("#view-expenses-submit-hidden").click();
+});
+
+document.querySelector("#view-expenses").addEventListener("submit", (event) => {
+    console.log("View submit registered");
     event.preventDefault();
     let startDate = event.target[0].value;
     let endDate = event.target[1].value;
     sqlDriver.getExpenses(startDate, endDate, (rows)=> {
         console.log(rows);
     });
+    document.querySelector("#view-expenses-modal").classList.remove("is-active");
+});
+
+document.querySelector("#enter-expense-button").addEventListener("click", () => {
+    document.querySelector("#enter-expense-modal").classList.add("is-active");
+});
+
+document.querySelector("#enter-expense-close").addEventListener("click", () => {
+    document.querySelector("#enter-expense-modal").classList.remove("is-active");
+});
+
+document.querySelector("#view-expenses-button").addEventListener("click", () => {
+    document.querySelector("#view-expenses-modal").classList.add("is-active");
+});
+
+document.querySelector("#view-expenses-close").addEventListener("click", () => {
+    document.querySelector("#view-expenses-modal").classList.remove("is-active");
 });
